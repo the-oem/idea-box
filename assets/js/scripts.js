6ffacf4004dc939f3431f2d1effa04c60a362f0c
Jason Collins <the.oem@gmail.com>
2017-04-05 16:51:19 -0600

    Added functionality to allow users to edit title and body inline after a card has been created. The changes are persisted to local storage.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,10 +10,10 @@ var qualityArray = ['swill', 'plausible', 'genius']
 
 // Constructors
 
-function Idea(title, body, id) {
+function Idea(title, body) {
 	this.title = title;
 	this.body = body;
-	this.id = id;
+	this.id = Math.round(Math.random() * Date.now());
 	this.quality = "swill";
 }
 
@@ -45,13 +45,33 @@ $body
 	.on('input', '.body-input', readyToSubmit)
 	.on('click', '.submit-btn', function () {
 		event.preventDefault();
-		var idea = new Idea($titleInput.val(), $bodyInput.val(), newGuid());
+		var idea = new Idea($titleInput.val(), $bodyInput.val());
 		storeIdea(idea);
 		prependIdea(idea);
 	})
 	.on('click', '.delete-btn', deleteIdea)
 	.on('click', '.upvote-btn', adjustQuality)
-	.on('click', '.downvote-btn', adjustQuality);
+	.on('click', '.downvote-btn', adjustQuality)
+	.on('input', '.idea-title', updateInline)
+	.on('input', '.idea-body', updateInline);
+
+function updateInline() {
+	var $updateElement = $(this).prop('class');
+	var $updateValue = $(this).text();
+	switch ($updateElement) {
+		case 'idea-title':
+			var $ideaId = $(this).parent().parent().prop('id');
+			updateIdea($ideaId, 'title', $updateValue);
+			break;
+		case 'idea-body':
+			var $ideaId = $(this).parent().prop('id');
+			updateIdea($ideaId, 'body', $updateValue);
+			break;
+		default:
+			console.log('Houston, we have a fucking problem while updating title or body.');
+	}
+	storeIdeas();
+}
 
 function deleteIdea() {
 	var $id = $(this).parent().parent().prop('id');
@@ -72,7 +92,7 @@ function adjustQuality() {
 			var newQuality = qualityArray[qualityArray.indexOf($currentQuality) - 1] || $currentQuality;
 			break;
 		default:
-			console.log('Houston, we have a fucking problem.');
+			console.log('Houston, we have a fucking problem while updating quality.');
 			break;
 	}
 	updatePageText($qualityElement, newQuality);
@@ -85,6 +105,7 @@ function updatePageText(element, value) {
 }
 
 function updateIdea(id, property, value) {
+	debugger;
 	var updatedList = ideaArray.map(function (idea) {
 		if (idea.id == id) {
 			idea[property] = value;
@@ -103,10 +124,6 @@ function removeFromStorage(id) {
 	localStorage.setItem('ideaBoxArray', JSON.stringify(ideaArray));
 }
 
-function newGuid() {
-	return Math.round(Math.random() * Date.now());
-}
-
 function storeIdea(idea) {
 	ideaArray.push(idea);
 	storeIdeas();
@@ -117,7 +134,6 @@ function storeIdeas() {
 }
 
 function prependIdea(idea) {
-	// debugger;
 	var $title = idea.title;
 	var $body = idea.body;
 	var $quality = idea.quality;
@@ -125,10 +141,10 @@ function prependIdea(idea) {
 	$(".card-listing").prepend(`
 	  <div class="card" id="${$id}">
 	    <div class="card-header">
-	      <h3 class="idea-title">${$title}</h3>
+	      <h3 class="idea-title" contenteditable="true">${$title}</h3>
 	      <button type="button" name="button" class="delete-btn" />
 	    </div>
-	    <p class="idea-body">${$body}</p>
+	    <p class="idea-body" contenteditable="true">${$body}</p>
 	    <div class="card-footer">
 	      <button type="button" name="button" class="upvote-btn" />
 	      <button type="button" name="button" class="downvote-btn" />
